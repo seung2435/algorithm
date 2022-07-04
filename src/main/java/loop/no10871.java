77af74c2eaf94177dccfb21a7ff2b92898d3f131
seung2435 <seung2435@naver.com>
2022-07-04 23:29:31 +0900

    [반복문] 10871.X보다 작은 수

--- src/main/java/loop/no10871.java
+++ src/main/java/loop/no10871.java
@@ -11,7 +11,6 @@ public class no10871 {
     * */
     public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
