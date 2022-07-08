5efbbd80af392fe404d46d39f2bd7a869d2f4fb7
seung2435 <seung2435@naver.com>
2022-07-08 23:38:39 +0900

    [반복문] 10951.A+B - 4

---
+++ src/main/java/loop/no10951.java
@@ -0,0 +1,25 @@
+package loop;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class no10951 {
+    /*
+    * 입력은 여러 개의 테스트 케이스로 이루어져 있다.
+    * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+    * */
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        while((st = new StringTokenizer(br.readLine())) != null){
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            System.out.println(A+B);
+
+        }
+
+    }
+}
