57550119c9edf9a7926ac587f5064477bfbff767
seung2435 <seung2435@naver.com>
2022-07-21 21:38:47 +0900

    [1차원배열] 8958.OX퀴즈

---
+++ src/main/java/array/no8958.java
@@ -0,0 +1,37 @@
+package array;
+
+import java.util.Scanner;
+
+public class no8958 {
+    /*
+    * "OOXXOXXOOO"와 같은 OX퀴즈의 결과가 있다. O는 문제를 맞은 것이고, X는 문제를 틀린 것이다.
+    * 문제를 맞은 경우 그 문제의 점수는 그 문제까지 연속된 O의 개수가 된다. 예를 들어, 10번 문제의 점수는 3이 된다.
+    * "OOXXOXXOOO"의 점수는 1+2+0+0+1+0+0+1+2+3 = 10점이다.
+    * OX퀴즈의 결과가 주어졌을 때, 점수를 구하는 프로그램을 작성하시오.
+    * */
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String arry[] = new String[sc.nextInt()];
+
+        for (int i=0; i< arry.length; i++){
+            arry[i] = sc.next();
+        }
+
+        for (int i=0; i< args.length; i++){
+            int cnt = 0;
+            int sum = 0;
+
+            for (int j=0; j<arry[i].length(); j++){
+                if (arry[i].charAt(j) == 'O'){
+                    cnt ++;
+                }else{
+                    cnt = 0;
+                }
+                sum += cnt;
+            }
+            System.out.println(sum);
+        }
+
+    }
+}
